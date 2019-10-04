f50be79e63ae60730f9eb0f899f66486cb676981
dimas eka adinandra <dimascapella1590@gmail.com>
2019-10-04 16:30:53 +0700

    finish Method Fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,17 +7,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.sql.ResultSet;
+
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
 import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
+import id.ac.polinema.idealbodyweight.fragments.ResultFragment;
 import id.ac.polinema.idealbodyweight.util.BrocaIndex;
 
-public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener, BrocaIndexFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener, BrocaIndexFragment.OnFragmentInteractionListener, ResultFragment.OnFragmentInteractionListener {
 
 	// Deklarasikan atribut Fragment di sini
 	private AboutFragment aboutFragment;
 	private MenuFragment menuFragment;
 	private BrocaIndexFragment brocaIndexFragment;
+	private ResultFragment resultFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 		setContentView(R.layout.activity_main);
 		menuFragment = new MenuFragment();
 		brocaIndexFragment = new BrocaIndexFragment();
+		resultFragment = new ResultFragment();
 		aboutFragment = AboutFragment.newInstance("Dimas Eka Adinandra");
 		getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, menuFragment).commit();
 	}
@@ -59,6 +64,12 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 
 	@Override
 	public void onCalculateBrocaIndexClicked(float index) {
-
+        resultFragment.setInformation(String.format("Your Ideal weight is %.2f kg", index));
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, resultFragment).commit();
 	}
+
+    @Override
+    public void onTryAgainButtonClicked(String tag) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, brocaIndexFragment).commit();
+    }
 }
